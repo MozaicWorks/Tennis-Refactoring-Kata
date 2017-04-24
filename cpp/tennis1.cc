15a63d2a14413d156eaa179929d1914ca23123fa
Alexandru Bolboaca <alexboly@gmail.com>
2017-04-24 14:06:34 +0300

    Simplify code for formatting score before advantages

--- cpp/tennis1.cc
+++ cpp/tennis1.cc
@@ -3,56 +3,51 @@
 
 std::string getEqualScoreName(int score);
 
+std::string getScoreAsString(int tempScore);
+
 const std::string tennis_score(int p1Score, int p2Score) {
     std::string score = "";
-    int tempScore=0;
+    int tempScore = 0;
     bool equalScore = (p1Score == p2Score);
-    if (equalScore)
-    {
+    if (equalScore) {
         score = getEqualScoreName(p1Score);
-    }
-    else if (p1Score>=4 || p2Score>=4)
-    {
-        int minusResult = p1Score-p2Score;
-        if (minusResult==1) score ="Advantage player1";
-        else if (minusResult ==-1) score ="Advantage player2";
-        else if (minusResult>=2) score = "Win for player1";
-        else score ="Win for player2";
-    }
-    else
-    {
-        for (int i=1; i<3; i++)
-        {
-            if (i==1) tempScore = p1Score;
-            else { score+="-"; tempScore = p2Score;}
-            switch(tempScore)
-            {
-                case 0:
-                    score+="Love";
-                    break;
-                case 1:
-                    score+="Fifteen";
-                    break;
-                case 2:
-                    score+="Thirty";
-                    break;
-                case 3:
-                    score+="Forty";
-                    break;
+    } else if (p1Score >= 4 || p2Score >= 4) {
+        int minusResult = p1Score - p2Score;
+        if (minusResult == 1) score = "Advantage player1";
+        else if (minusResult == -1) score = "Advantage player2";
+        else if (minusResult >= 2) score = "Win for player1";
+        else score = "Win for player2";
+    } else {
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) tempScore = p1Score;
+            else {
+                score += "-";
+                tempScore = p2Score;
             }
+            score += getScoreAsString(tempScore);
         }
     }
     return score;
 }
 
+std::string getScoreAsString(int tempScore) {
+    std::map<int, std::string> scoresToStrings = {
+            {0, "Love"},
+            {1, "Fifteen"},
+            {2, "Thirty"},
+            {3, "Forty"}
+    };
+    return scoresToStrings[tempScore];
+}
+
 std::string getEqualScoreName(int score) {
-    static std::map<int,std::string> equalScoreValuesToStrings = {
+    static std::map<int, std::string> equalScoreValuesToStrings = {
             {0, "Love-All"},
             {1, "Fifteen-All"},
             {2, "Thirty-All"},
             {3, "Deuce"}
     };
 
-    if(equalScoreValuesToStrings.find(score) == equalScoreValuesToStrings.end()) return "Deuce";
+    if (equalScoreValuesToStrings.find(score) == equalScoreValuesToStrings.end()) return "Deuce";
     return equalScoreValuesToStrings[score];
 }
