1a64a2551c23143ea82ceeffc1f153d689a850cd
Alexandru Bolboaca <alexboly@gmail.com>
2017-04-27 09:36:21 +0300

    Extract tennis.h

---
+++ cpp/tennis.h
@@ -0,0 +1,13 @@
+#ifndef CPP_TENNIS_H
+#define CPP_TENNIS_H
+
+#include <string>
+
+static const std::string SEPARATOR = "-";
+
+static const std::string DEFAULT_SCORE_FORMAT = "";
+
+
+const std::string tennis_score(int playerOneScore, int playerTwoScore);
+
+#endif //CPP_TENNIS_H
